56f902455fc5932a23848a524dde25e073f7ed75
Bia <wranabia@gmail.com>
2026-07-20 21:44:01 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(
+    Center(
+      child: Text(
+        'Ola eu sou a Tonani',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 30.0),
+      ),
+    ),
+  );
+}
