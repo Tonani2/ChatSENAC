a67b68ce7b4a208a8e50150bb7dc0eb9dbc8814d
Tonani <tonanianaluiza2@gmail.com>
2026-07-27 21:33:14 -0300

    finaliza a tela de login - adiciona espaços com SizedBox - adiciona padding ao redor dos elementos - estiliza os botões com o style

--- lib/main.dart
+++ lib/main.dart
@@ -20,48 +20,91 @@ class MyApp extends StatelessWidget {
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Row(children: [FlutterLogo(size: 18), Text("ChatSENAC")]),
-        SizedBox(height: 32),
+    return Padding(
+      padding: EdgeInsets.all(24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(children: [FlutterLogo(size: 18), Text("ChatSENAC")]),
 
-        //Títulos
-        Text("Entre na sua conta", style: Tipografia.h1),
-        Text(
-          "Coloque o seu email e senha para logar",
-          style: Tipografia.subtitulo,
-        ),
+          SizedBox(height: 32),
 
-        //Campos
-        Text("Email", style: Tipografia.subtitulo),
-        TextField(),
-        Text("Senha", style: Tipografia.subtitulo),
-        TextField(),
-        InkWell(
-          child: Text(
-            "Esqueceu a senha?",
-            textAlign: TextAlign.right,
-            style: Tipografia.link,
+          //Títulos
+          Text("Entre na sua conta", style: Tipografia.h1),
+          SizedBox(height: 22),
+          Text(
+            "Coloque o seu email e senha para logar",
+            style: Tipografia.subtitulo,
           ),
-        ),
+          SizedBox(height: 20),
 
-        //Botões
-        ElevatedButton(onPressed: null, child: Text("Entrar")),
-        Text("Ou", textAlign: TextAlign.center),
-        ElevatedButton(onPressed: null, child: Text("Continuar com o Google")),
-        ElevatedButton(
-          onPressed: null,
-          child: Text("Continuar com o Facebook"),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Não tem uma conta!", style: Tipografia.subtitulo),
-            InkWell(child: Text("Cadastre-se", style: Tipografia.link)),
-          ],
-        ),
-      ],
+          //Campos
+          Text("Email", style: Tipografia.subtitulo),
+          TextField(decoration: InputDecoration(border: OutlineInputBorder())),
+          SizedBox(height: 19),
+          Text("Senha", style: Tipografia.subtitulo),
+          TextField(decoration: InputDecoration(border: OutlineInputBorder())),
+          SizedBox(height: 10),
+          InkWell(
+            child: Text(
+              "Esqueceu a senha?",
+              textAlign: TextAlign.right,
+              style: Tipografia.link,
+            ),
+          ),
+          SizedBox(height: 20),
+
+          //Botões
+          ElevatedButton(onPressed: (){},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+                  child: Text("Entrar"),
+              ),
+
+
+          SizedBox(height: 20),
+          Text("Ou", textAlign: TextAlign.center),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: (){},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Text("Continuar com o Google"),
+          ),
+          SizedBox(height: 9),
+          ElevatedButton(
+            onPressed: (){},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Text("Continuar com o Facebook"),
+          ),
+          SizedBox(height: 90),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Não tem uma conta?", style: Tipografia.subtitulo),
+              SizedBox(width: 6,),
+
+              InkWell(child: Text("Cadastre-se", style: Tipografia.link)),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
